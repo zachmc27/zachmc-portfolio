fec43c2af3afc5a6531d16f1ad9ec78f6e9f4ec6
zachmc27 <dzdudes1213@gmail.com>
2025-03-17 22:43:56 -0500

    fixed menu bars on pgone version

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,14 +2,14 @@ import { Button, Menu, Portal } from "@chakra-ui/react"
 import React from 'react'
 import Navbar from "./Navbar.jsx"
 import "../styles/Component-Styles/Menu.css"
-
+import bars from "../../assets/images/bars-solid.svg"
 
 export default function Dropdown() {
   return (
      <Menu.Root>
           <Menu.Trigger asChild>
             <Button variant="outline" size="sm" className="menu-btn">
-              <img src="../../assets/images/bars-solid.svg" alt="menu-bars" className="menu-bars"/>
+              <img src={`${bars}`} alt="menu-bars" className="menu-bars"/>
             </Button>
           </Menu.Trigger>
           <Portal>
